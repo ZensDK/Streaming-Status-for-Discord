a7e4ba8d1a204d0d5b437d7b197e6596a4ce4d89
zens <99699841+ZensDK@users.noreply.github.com>
2022-09-06 09:40:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import commands
-from colorama import Fore
 import asyncio
 from webserver import keep_alive
 import os
-
 my_secret = os.environ['Token']
 
 prefix = ">"
@@ -18,7 +16,7 @@ bot = commands.Bot(command_prefix=prefix,
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity = discord.Streaming(name = "Stream Name" , url = "https://twitch.tv/your-twitch")
- 
-                              
+    await bot.change_presence(status=discord.Status.online, activity = discord.Streaming(name = "Stream Subject" , url = "https://twitch.tv/username"))
+    print("Logging Succesful!")
+  
 bot.run(os.getenv('Token'), bot=False)
